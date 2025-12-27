d5161bdeb046957cbd3a35d37f35c44c7d2f12b6
Narendran <89729370+Narendran3678@users.noreply.github.com>
2025-12-27 20:36:16 +0530

    Update buildAndPackage.groovy

--- vars/buildAndPackage.groovy
+++ vars/buildAndPackage.groovy
@@ -1,11 +1,9 @@
 def call(Map config) {
     node {
         echo "Maven Build Started...${config.projectName}"
-        dir(${config.projectName}) {
-            bat '''
-            cd ${projectName}
-            mvn clean install -DskipTests=true versions:set -DnewVersion='%version%' versions:update-child-modules
-            '''
-        }
+        bat """
+        cd "${config.projectName}"
+        mvn clean install -DskipTests=true versions:set -DnewVersion='%version%' versions:update-child-modules
+        """
     }
 }
